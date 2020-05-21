fb1271dac1ae4bdfab2f6055c35bfc948673d664
WooKyungjin <wkj1103@naver.com>
2020-05-22 00:12:13 +0900

    baekjoon_2750

--- baekjoon_2750/baekjoon_2750.py
+++ baekjoon_2750/baekjoon_2750.py
@@ -7,12 +7,12 @@ n = int(input())
 for i in range(0, n):
     input_num = int(input())
     if input_num >= 0:
-        num_list[input_num] += 1
+        num_list[input_num+1000] += 1
     else:
-        num_list
+        num_list[1000-abs(input_num)] += 1
 
 for i in range(0, len(num_list)):
     if num_list[i] == 1:
-        print(i)
+        print(i-1000)
 
 
