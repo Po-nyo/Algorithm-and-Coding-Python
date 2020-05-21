7de22e9004059d0a2a9b9af9a0cffa8fcb19f5cd
WooKyungjin <wkj1103@naver.com>
2020-05-21 23:57:58 +0900

    baekjoon_2750

---
+++ baekjoon_2750/baekjoon_2750.py
@@ -0,0 +1,18 @@
+# [백준 2750] 수 정렬하기
+
+num_list = [0]*2001
+
+n = int(input())
+
+for i in range(0, n):
+    input_num = int(input())
+    if input_num >= 0:
+        num_list[input_num] += 1
+    else:
+        num_list
+
+for i in range(0, len(num_list)):
+    if num_list[i] == 1:
+        print(i)
+
+
