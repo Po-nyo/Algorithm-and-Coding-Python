9705c5c1b961781739bd544598294fb2430a0ee5
WooKyungjin <wkj1103@naver.com>
2020-05-23 22:22:00 +0900

    baekjoon_10989

--- baekjoon_10989/baekjoon_10989.py
+++ baekjoon_10989/baekjoon_10989.py
@@ -8,7 +8,7 @@ num_list = [0]*10001
 n = int(input())
 
 for i in range(0, n):
-    num_list[int(input())] += 1;
+    num_list[int(input())] += 1
 
 for i in range(1, len(num_list)):
     for j in range(0, num_list[i]):
